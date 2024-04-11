80f085d0503050c63447b44a7aebb4c1642af552
Karol <agnus.mcfife@gmail.com>
2024-04-11 12:39:53 +0200

    opencv lib setup

--- table-calibrator/src/table-calibrator.cpp
+++ table-calibrator/src/table-calibrator.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <opencv2/videoio.hpp>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
-}
+    if (argc != 2) {
+        std::cerr << "Error: Video path not provided." << std::endl
+            << "Usage: " << argv[0] << " <video_file>" << std::endl;
+        return -1;
+    }
 
+    cv::VideoCapture video(argv[1]);
+
+    if (!video.isOpened()) {
+        std::cerr << "Error: Couldn't open file " << argv[1];
+        return -1;
+    }
+
+    return 0;
+}
