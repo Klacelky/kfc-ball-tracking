2fb790ee93ed47202b2a449ecb251581f19c1856
Karol <agnus.mcfife@gmail.com>
2024-04-12 12:14:43 +0200

    feat: selection restart, improved help
    calibration.cpp while loops refactored, goto restart added

--- table-calibrator/src/calibration.cpp
+++ table-calibrator/src/calibration.cpp
@@ -4,55 +4,69 @@
 #include <iostream>
 
 #define WINDOW_NAME         "Calibration"
-#define WINDOW_NAME         "Calibration"
-#define HELP_TOP_LEFT       "[LMB] Select top left"
-#define HELP_TOP_RIGHT      "[LMB] Select top right"
-#define HELP_BOT_LEFT       "[LMB] Select bottom left"
-#define HELP_BOT_RIGHT      "[LMB] Select bottom right"
-#define HELP_CONSOLE_INPUT  "[CONSOLE] Enter table dimensions"
+#define HELP_TOP_LEFT       "[LMB] Select top left "
+#define HELP_TOP_RIGHT      "[LMB] Select top right "
+#define HELP_BOT_LEFT       "[LMB] Select bottom left "
+#define HELP_BOT_RIGHT      "[LMB] Select bottom right "
+#define HELP_REST_SELECT    "[R] Restart selection "
+#define HELP_SAVE_QUIT      "[S] Save and quit "
+#define HELP_QUIT           "[Q] Quit "
+#define HELP_CONTINUE       "[ENTER] Continue "
+#define HELP_CONSOLE_INPUT  "[CONSOLE] Enter table dimensions "
 
 void start_calibration(cv::VideoCapture video)
 {
-    cv::Mat frame;
+    const char* const HELP[6] = { HELP_REST_SELECT HELP_TOP_LEFT, 
+                                  HELP_REST_SELECT HELP_TOP_RIGHT,
+                                  HELP_REST_SELECT HELP_BOT_LEFT, 
+                                  HELP_REST_SELECT HELP_BOT_RIGHT,
+                                  HELP_REST_SELECT HELP_CONTINUE,
+                                  HELP_REST_SELECT HELP_SAVE_QUIT HELP_QUIT};
+
     MouseData mdata;
-    cv::Point2f src[4];
-    char selected = 0;
-
-    const char* const HELP[5] = { HELP_TOP_LEFT, HELP_TOP_RIGHT,
-                                  HELP_BOT_LEFT, HELP_BOT_RIGHT,
-                                  HELP_CONSOLE_INPUT};
-
     cv::namedWindow(WINDOW_NAME);
     cv::setMouseCallback(WINDOW_NAME, mouse_controller, &mdata);
 
+    cv::Mat frame;
+    cv::Point2f src[4];
+    char selected = 0;
+selection_start:
+    selected = 0;
+    mdata.clicked = false;
     while (true)
     {
-        if (selected == 4) {
-            break;
-        }
+
         if (!video.read(frame)) {
             continue;
         }
-        if (mdata.clicked) {
+
+        if (mdata.clicked && selected < 4) {
             src[selected++] = mdata.pos;
             mdata.clicked = false;
         }
+
         draw_points(frame, src, selected);
         draw_help(frame, HELP[selected]);
         cv::imshow(WINDOW_NAME, frame);
-        cv::waitKey(10);
+
+        int key = cv::waitKey(10);
+        if ('r' == key) {
+            goto selection_start;
+        }
+        if (4 == selected && 13 == key) {   // enter
+            break;
+        }
     }
 
     float width, height;
-    std::cout << "Enter table width in milimeters:" << std::endl;
-    std::cin >> width;
-    std::cout << "Enter table height in milimeters:" << std::endl;
-    std::cin >> height;
+    std::cout << "Enter table width in milimeters: ";   
+    std::cin  >> width;
+    std::cout << "Enter table height in milimeters: ";  
+    std::cin  >> height;
     cv::Point2f dst[4] = { {0,0}, {width, 0}, {0, height}, {width, height} };
-
     const cv::Mat PERSP_MAT = cv::getPerspectiveTransform(src, dst);
-    cv::Mat wframe;
 
+    cv::Mat wframe;
     while (true)
     {
         if (!video.read(frame)) {
@@ -60,8 +74,18 @@ void start_calibration(cv::VideoCapture video)
         }
         cv::warpPerspective(frame, wframe, PERSP_MAT, cv::Size(width, height), cv::INTER_NEAREST);
         cv::resize(wframe, wframe, cv::Size(frame.rows * width / height, frame.rows), cv::INTER_NEAREST);
+        draw_help(wframe, HELP[5]);
         cv::imshow(WINDOW_NAME, wframe);
-        cv::waitKey(10);
+        int key = cv::waitKey(10);
+        if ('r' == key) {
+            goto selection_start;
+        }
+        if ('s' == key) {
+            continue;
+        }
+        if ('q' == key) {
+            return;
+        }
     }
 }
 
@@ -85,7 +109,7 @@ void draw_points(cv::Mat& frame, cv::Point2f points[], int count)
 
 void draw_help(cv::Mat &frame, const char* str)
 {
-    static const cv::Rect bg(0, 0, 400, 40);
+    static const cv::Rect bg(0, 0, 500, 40);
     static const cv::Scalar WHITE(0, 0, 0);
     static const cv::Scalar BLACK(255, 255, 245);
     cv::rectangle(frame, bg, WHITE, cv::FILLED);
